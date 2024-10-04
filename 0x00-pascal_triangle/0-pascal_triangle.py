494d83ce263a2b1e8d1a9047e961ff9916d7f8ec
martin olutade <112440359+silgenius@users.noreply.github.com>
2024-10-04 03:30:19 +0100

    Create 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Generate the first n rows of Pascal's triangle
+"""
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+  
+    tri = []
+    for i in range(n):
+        row = [1] * (i + 1)  # Initialize a row with 1s
+        for j in range(1, i):
+            row[j] = tri[i - 1][j - 1] + tri[i - 1][j]  # Compute values based on the previous row
+        tri.append(row)
+    
+    return tri
