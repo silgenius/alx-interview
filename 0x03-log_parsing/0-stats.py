a4298990ee2b37943cbdd8e3fc7755c91b0d380d
silgenius <olutademartin@gmail.com>
2024-10-25 15:12:58 +0000

    Commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 
 """
 a script that reads stdin line by line and computes metrics
@@ -6,24 +6,52 @@ a script that reads stdin line by line and computes metrics
 
 import re
 import signal
-
+import sys
 import re
+import signal
+import os
 
-pattern = re.compile(r"""
-    (\d{1,3}\.){3}\d{1,3}            # Match an IP address
-    \s- \[                            # Match the space and the opening bracket
-    \d{4}(-\d{2}){2}                 # Match the date in YYYY-MM-DD format
-    \s(\d{2}:){2}\d{2}\.\d{6}         # Match the time in HH:MM:SS.ssssss format
-    \]                                 # Match the closing bracket
-    "GET /projects/260 HTTP/1.1"      # Match the HTTP request line
-    \s[2-5][0][0-145]                 # Match the status code (excluding 2)
-    \s\d{1,4}                         # Match a number (1 to 4 digits)
-""", re.VERBOSE)
+pattern = r'(\d{1,3}\.){3}\d{1,3} - \[\d{4}(-\d{2}){2} (\d{2}:){2}\d{2}\.\d{6}\] "GET /projects/260 HTTP/1.1" [2-5][0][0-1345] \d{1,4}'
 
+def print_stats(total_size, stats):
+    print(f'File size: {total_size}')
+    for key, value in stats.items():
+        print(f'{key}: {value}')
 
-while (True):
-    test_string = input()
-    match = pattern.match(test_string)
+def print_metrics():
+    count = 0
+    total_size = 0
+    stats = {}
 
-if match:
-    print("okay")
+try:
+    for line in sys.stdin:
+        match = re.match(pattern, line)
+        try:
+            match.group()
+
+            # Extract status code
+            check = r'[2-5][0][0-1345]'
+            status_code = re.search(check, line)
+            status_code = int(status_code.group())
+
+            # Extract File size
+            check = r'\d{1,4}'
+            file_size = re.search(check, line)
+            file_size = file_size.group()
+
+            if count < 10:
+                if not stats.get(status_code):
+                    stats[status_code] = 1
+                else:
+                    stats[status_code] += 1
+        
+            count += 1
+            total_size += int(file_size)
+
+            if count == 9:
+                print_stats(total_size, stats)
+                count = 0
+        except AttributeError:
+            pass
+except KeyboardInterrupt:
+    print_stats(total_size, stats)
