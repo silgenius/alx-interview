0d7637d6e71c3721df716522acee8e05375a3051
silgenius <olutademartin@gmail.com>
2024-11-20 15:15:46 +0100

    Properly document file

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -18,9 +18,12 @@ def rotate_2d_matrix(matrix):
     rows = len(matrix)
     cols = len(matrix[0])
 
+    # Rotate matrix by 90 degree
     rotate90 = [
             [matrix[row][i] for row in range(rows - 1, -1, -1)]
             for i in range(cols)]
+
+    # Copy matrix
     for i in range(rows):
         for j in range(cols):
             matrix[i][j] = rotate90[i][j]
