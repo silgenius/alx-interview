a1919eaa09578e9de344cb6bb3bba797ad6676e8
silgenius <olutademartin@gmail.com>
2024-11-20 14:06:27 +0000

    Fix requirement error

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 
+"""
+Contains the function rotate_2d_matrix
+"""
+
+
 def rotate_2d_matrix(matrix):
     """
     Rotates a 2D matrix 90 degrees clockwise in-place.
-    
+
     Args:
     matrix (List[List[int]]): The 2D matrix to be rotated.
-    
+
     Returns:
     None: The matrix is modified in-place.
     """
     rows = len(matrix)
     cols = len(matrix[0])
-    
-    rotate90 = [[matrix[row][i] for row in range(rows - 1, -1, -1)] for i in range(cols)]
+
+    rotate90 = [
+            [matrix[row][i] for row in range(rows - 1, -1, -1)]
+            for i in range(cols)]
     for i in range(rows):
         for j in range(cols):
             matrix[i][j] = rotate90[i][j]
