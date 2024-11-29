6360cc0185db8d62d4c27c5b49eeaf8cd3b59958
silgenius <olutademartin@gmail.com>
2024-11-29 14:22:46 +0100

    Revert changes made to file

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -22,7 +22,9 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for i in range(1, total + 1):
-        dp[i] = min(dp[i], *[dp[i - coin] + 1 if i - coin >= 0 else float('inf') for coin in coins])
+    for coin in coins:
+        for t in range(coin, total + 1):
+            if dp[t - coin] != float('inf'):
+                dp[t] = min(dp[t], dp[t - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
