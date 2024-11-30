f2ce397826f128435b5c4f8da63a0c11dd381fef
silgenius <olutademartin@gmail.com>
2024-11-30 16:40:55 +0100

    Implement solution to task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
 
 """
-Generate the first n rows of Pascal's triangle
+Contain a function def pascal_triangle(n): that returns a list of
+lists of integers representing the Pascal’s triangle of n
 """
 
+
 def pascal_triangle(n):
+    """Generate the first n rows of Pascal's triangle
+
+    Args:
+        n (int): amount or rows to generate
+
+    Returns:
+        List[List(int)]: _description_
+    """
     if n <= 0:
         return []
-  
     tri = []
-    for i in range(n):
-        row = [1] * (i + 1)  # Initialize a row with 1s
-        for j in range(1, i):
-            row[j] = tri[i - 1][j - 1] + tri[i - 1][j]  # Compute values based on the previous row
-        tri.append(row)
-    
+
+    tri.append([1])
+    if n == 1:
+        return tri
+
+    tri.append([1, 1])
+    if n == 2:
+        return tri
+
+    for i in range(3, n + 1):
+        x = [float('inf')] * i
+        x[0] = 1
+        x[-1] = 1
+        for j in range(len(tri[i - 2]) - 1):
+            x[j + 1] = tri[i - 2][j] + tri[i - 2][j + 1]
+        tri.append(x)
+        x = []
     return tri
