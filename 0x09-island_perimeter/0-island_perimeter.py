26536a83ddf14994e3ab6d66537f84bdc658dce8
silgenius <olutademartin@gmail.com>
2024-12-05 16:57:41 +0100

    Implement solution to task v.0.1

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -24,11 +24,16 @@ def island_perimeter(grid) -> int:
     """
 
     perimeter = 0
-    for row in range(1, len(grid) - 1):
-        for cell in range(1, len(grid[row]) - 1):
-            if grid[row][cell] == 1:
-                perimeter += helper_cell(grid[row][cell - 1])
-                perimeter += helper_cell(grid[row][cell + 1])
-                perimeter += helper_cell(grid[row - 1][cell])
-                perimeter += helper_cell(grid[row - 1][cell])
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:  # If the cell is land
+                # Check the four possible directions (up, down, left, right)
+                if i == 0 or grid[i - 1][j] == 0:  # Check top
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:  # Check bottom
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:  # Check left
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:  # Check right
+                    perimeter += 1
     return perimeter
